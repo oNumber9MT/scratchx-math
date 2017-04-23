673ba464954300a114b85540be1a9776046c359e
Masashi Takeshita <masatake227@hotmail.com>
2017-04-23 19:57:28 +0900

    add min, max, sort

--- math_extension.js
+++ math_extension.js
@@ -41,6 +41,54 @@
     callback(sum/array.length);
   };
 
+  // 最小値
+  ext.getMin = function(sep, arr, callback) {
+    var array = arr.split(sep);
+    var array_out = arraySortAsc(array);
+    callback(array_out[0]);
+  }
+
+  // 最大値
+  ext.getMax = function(sep, arr, callback) {
+    var array = arr.split(sep);
+    var array_out = arraySortAsc(array);
+    callback(array_out[array_out.length - 1]);
+  }
+
+  // ソート（昇順）
+  ext.sortAsc = function(sep, arr, callback) {
+    var array = arr.split(sep);
+    var array_out = arraySortAsc(array);
+    callback(array_out);
+  }
+
+  // ソート（降順）
+  ext.sortDesc = function(sep, arr, callback) {
+    var array = arr.split(sep);
+    var array_out = arraySortDesc(array);
+    callback(array_out);
+  }
+
+  // 配列ソート関数（昇順）
+  function arraySortAsc(arr) {
+    arr.sort(function(a, b) {
+      if(a < b) return -1;
+      if(a > b) return 1;
+      return 0;
+    });
+    return arr; 
+  }
+
+  // 配列ソート関数（降順）
+  function arraySortDesc(arr) {
+    arr.sort(function(a, b) {
+      if(a > b) return -1;
+      if(a < b) return 1;
+      return 0;
+    });
+    return arr; 
+  }
+
   var descriptor = {
     blocks: [
       ['R', 'π', 'getPI'],
@@ -48,6 +96,10 @@
       ['R', '%n の %n 乗', 'getPow', 2, 3],
       ['R', '%s 区切りの数値データ %s の合計', 'getSum', ','],
       ['R', '%s 区切りの数値データ %s の平均', 'getAve', ','],
+      ['R', '%s 区切りの数値データ %s の最小値', 'getMin', ','],
+      ['R', '%s 区切りの数値データ %s の最大値', 'getMax', ','],
+      ['R', '%s 区切りのデータ %s を昇順でソートしたもの', 'sortAsc', ','],
+      ['R', '%s 区切りのデータ %s を降順でソートしたもの', 'sortDesc', ','],
     ]
   };
 
